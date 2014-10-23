b2c916aefded9d8fdd85134793e5d0a8acdeffc7
Marcin Jedrzejewski <marcinje@gmail.com>
2014-10-23 09:59:39 +0200

    Added chapter 10

---
+++ ch10/ch10.cpp
@@ -0,0 +1,29 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include <windows.h>
+
+class Outer {
+	int b;
+
+	class Inner {
+	public:
+		int a;
+
+		Inner(Outer* p) {
+			p->b = 12;
+		}
+
+	};
+
+	void call(Inner* p) {
+		p->a = 1;
+	}
+
+};
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
+
