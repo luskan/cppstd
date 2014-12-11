dad21f67382dc2555dd9dae0dfa2f87e6b173acd
Marcin Jędrzejewski <marcinje@gmail.com>
2014-12-11 11:02:51 +0100

    templ ch4 + regexp sample from so

--- regex/main.cpp
+++ regex/main.cpp
@@ -3,7 +3,32 @@
 #include <iostream>
 using namespace std;
 
+void SoTest1() {
+	std::string s("aaabbbccd123456eeffgg");
+	std::smatch match;
+	std::regex braced_regex("([a-zA-Z_]+)(\\d{2,})(\\w+)");
+	std::regex plus_regex("(\\w+)(\\d+)(\\w+)");
+
+	auto printer = [](decltype(*match.begin())& match) {
+		std::ssub_match sub(match);
+		std::string match_substring(sub.str());
+		std::cout << match_substring << '\n';
+	};
+
+	std::regex_match(s, match, braced_regex);
+	std::cout << "Number of braced matches: " << match.size() << '\n';
+	std::for_each(match.begin(), match.end(), printer);
+
+	std::regex_match(s, match, plus_regex);
+	std::cout << "Number of plus matches: " << match.size() << '\n';
+	std::for_each(match.begin(), match.end(), printer);
+	return;
+}
+
 int main() {
+
+	SoTest1();
+
 	regex  reg("[0-9]+");
 	if (regex_match("123000", reg)) {
 		cout << "It's a match!" << endl;
---
+++ templ_ch4/templ_ch4.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <memory>
+#include <array>
+#include <functional>
+
+
+int main()
+{
+
+}
