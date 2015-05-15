7fa7c3357e409936363b91f5ddda027a341561a9
Marcin Jędrzejewski <marcinje@gmail.com>
2015-05-15 10:41:50 +0200

    fix

--- templ_ch12/transl1.cpp
+++ templ_ch12/transl1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "templ.h"
 
+// under vs2013 default ctor gets caed
+// under gcc5.1.0 no ctor gets called, its nondefining
 template<>
-CallCtr CallHolder<CallCtr>::tt(1); // under gcc its nondefining
+CallCtr CallHolder<CallCtr>::tt;
 
 void CallGrade()
 {
