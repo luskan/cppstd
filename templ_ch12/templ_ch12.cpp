e9e46cfb397df1201251b040f706cce29edda086
Marcin Jędrzejewski <marcinje@gmail.com>
2015-05-15 09:10:38 +0200

    Test case for 12.3.3

--- templ_ch12/templ_ch12.cpp
+++ templ_ch12/templ_ch12.cpp
@@ -6,14 +6,34 @@
 
 #include "templ.h"
 
-extern void CallGrade();
 
+// For 12.3.3
+class CallCtr {
+public:
+	CallCtr() {
+		std::cout << "CallCtr::CallCtr()" << std::endl;
+	}
+private:
+	CallCtr(CallCtr const&);
+};
+
+template<typename T>
+class CallHolder {
+public:
+	static T tt;
+};
+
+template<>
+CallCtr CallHolder<CallCtr>::tt;
+
+extern void CallGrade();
 int main()
 {
 	MyGrade<int> grade;
 	int nn = grade.value;
 	std::cout << "From templ_ch12.cpp:: " << nn << std::endl;
-
-
 	CallGrade();
+
+	// 12.3.3 Full Member Specialization
+	CallHolder<CallCtr> test;
 }
