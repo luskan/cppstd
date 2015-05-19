9e8c6c9d32e7e3ebceac28329c3a8053736bae6f
Marcin Jędrzejewski <marcinje@gmail.com>
2015-05-19 22:02:24 +0200

    ch13 init

---
+++ templ_ch13/templ_ch13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <list>
+
+template<typename T1=insqdwdas, typename T2=khjkhjhk>
+int countr(T1 a, T2 b) {
+	return a + b + 1;
+}
+
+int main()
+{
+	std::cout << countr(1, 2) << std::endl;
+}
